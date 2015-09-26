d2c9dd16b295275f9d55cb8facae35d381d838d7
Benjamin San Souci <benjamin.sansouci@gmail.com>
2015-09-26 16:37:44 -0700

    Use es6 multiline string.

--- scripts/deploy-ghpages.js
+++ scripts/deploy-ghpages.js
@@ -1,11 +1,9 @@
 var ghpages = require('gh-pages');
 var fs = require('fs');
-main();
-function main() {
-    var str =
-    "<!doctype html><html><head><title>ffffffffffffffff</title>"+
-    "<style type='text/css'> body { margin:0; } </style></head><body>"+
-    "<div id='root'></div><script src='./bundle.js'></script></body></html>";
-    fs.writeFileSync('./dist/index.html', str);
-    ghpages.publish('./dist', console.error.bind(console));
-}
+var str = `
+  <!doctype html><html><head><title>ffffffffffffffff</title>
+  <style type='text/css'> body { margin:0; } </style></head><body>
+  <div id='root'></div><script src='./bundle.js'></script></body></html>
+`;
+fs.writeFileSync('./dist/index.html', str);
+ghpages.publish('./dist', console.error.bind(console));
