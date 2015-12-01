35accc5abcbb200884bba7ba78b178e89b3559a8
Schmavery <avery.schmavery@gmail.com>
2015-11-30 16:37:15 -0800

    Compress images on upload

--- src/profile.js
+++ src/profile.js
@@ -22,13 +22,43 @@ export class Profile extends Component{
   }
 
   handleImage(e){
+    var MAX_WIDTH = 200;
+    var MAX_HEIGHT = 200;
     var self = this;
     var reader = new FileReader();
     var file = e.target.files[0];
     reader.onload = (upload) => {
-      console.log("HI", upload.target.result);
+      var canvas = document.createElement('canvas');
+      canvas.style.display = 'none';
+      document.body.appendChild(canvas);
+      var img = document.createElement("img");
+      img.src = upload.target.result;
+      var ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      var width = img.width;
+      var height = img.height;
+      if (width > height) {
+        if (width > MAX_WIDTH) {
+          height *= MAX_WIDTH / width;
+          width = MAX_WIDTH;
+        }
+      } else {
+        if (height > MAX_HEIGHT) {
+          width *= MAX_HEIGHT / height;
+          height = MAX_HEIGHT;
+        }
+      }
+      canvas.width = width;
+      canvas.height = height;
+      ctx.drawImage(img, 0, 0, width, height);
+
+      var dataUrl = canvas.toDataURL("image/png");
+
+      canvas.parentNode.removeChild(canvas);
+
+      console.log("DataUrl:", typeof dataUrl, "-"+dataUrl+"-", dataUrl.length, upload.target.result.length);
       var accts = this.props.fns.getAccounts();
-      accts.forEach(acct => acct === this.props.fns.getAccount() ? acct.image = upload.target.result : null);
+      accts.forEach(acct => acct === this.props.fns.getAccount() ? acct.image = dataUrl : null);
       this.props.fns.deepUpdate({'accounts':{$set: accts}});
     }
     reader.readAsDataURL(file);
